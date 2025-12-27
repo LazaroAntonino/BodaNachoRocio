779fe970a8212bc9717884194bdcdc7cff85c214
Antonino Lázaro <antoninolazaro@X21088.local>
2025-12-27 18:59:43 +0100

    Commit inicial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { useNavigate } from "react-router";
+
+export const Home = () => {
+
+
+	return (
+		<div className="home-container">
+			Hola esto funciona
+		</div>
+	);
+};
